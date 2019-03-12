c16c224285319430fff58fb8c9bc80923fbcec22
Malcolm <malcolm.maclure@gmail.com>
2019-03-12 19:21:00 -0400

    paddles

--- main.lua
+++ main.lua
@@ -7,6 +7,10 @@ VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
 function love.load()
+  love.graphics.setDefaultFilter('nearest', 'nearest')
+  smallFont = love.graphics.newFont('font.ttf', 8)
+  love.graphics.setFont(smallFont)
+
   push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
     fullscreen = false,
     resizable = false,
@@ -29,9 +33,14 @@ end
 function love.draw()
   push:apply('start')
 
+  love.graphics.clear(40, 45, 52, 255)
   love.graphics.printf(
-    'hello poooong', 0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, 'center'
+    'hello poooong', 0, 20, VIRTUAL_WIDTH, 'center'
   )
+  love.graphics.rectangle('fill', 10, 30, 5, 20)
+  love.graphics.rectangle('fill', VIRTUAL_WIDTH -10, VIRTUAL_HEIGHT - 50, 5, 20)
+  love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4,4)
 
   push:apply('end')
+
 end
